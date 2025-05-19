42c2ba572ce9d95217b30766820ab3eb2eb0d2c5
Maria Clara <mariaclara.dg25@gmail.com>
2025-05-19 11:57:45 -0300

    Criando uma nova tela

---
+++ AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetalheViewController.swift
+//  AluraViagens
+//
+//  Created by Maria Clara Dias on 19/05/25.
+//
+
+import UIKit
+
+class DetalheViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- AluraViagens/View/ViewController.swift
+++ AluraViagens/View/ViewController.swift
@@ -64,6 +64,11 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detalheController = DetalheViewController(nibName: "DetalheViewController", bundle: nil)
+        navigationController?.pushViewController(detalheController, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0{
             
