53ef1cb2ed08e9b6f740e47d42fdf378f4302258
Maria Clara <mariaclara.dg25@gmail.com>
2025-05-20 10:29:24 -0300

    Criando view de detalhes

--- AluraViagens/View/DetalheViewController/DetalheViewController.swift
+++ AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class DetalheViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var viagemImage: UIImageView!
+    @IBOutlet weak var tituloViagemLabel: UILabel!
+    @IBOutlet weak var subtituloViagemLabel: UILabel!
+    
+    //MARK: - View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    //MARK: - Actions
 
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
 }
